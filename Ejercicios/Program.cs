0632094f3eb3ca519509d2156ccb1f55de8d45ae
202402145-crypto <202402145@umh.edu.hn>
2025-08-06 08:18:11 -0600

    Add files via upload

---
+++ Ejercicios/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main()
+    {
+        // Declaración de variables
+        double costoHora, horasTrabajadas, totalDevengado;
+
+        // Solicitar datos al usuario
+        Console.Write("Ingrese el costo por hora trabajada: ");
+        costoHora = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Ingrese la cantidad de horas trabajadas: ");
+        horasTrabajadas = Convert.ToDouble(Console.ReadLine());
+
+        // Calcular el salario semanal
+        totalDevengado = costoHora * horasTrabajadas;
+
+        // Mostrar el resultado
+        Console.WriteLine("\nEl salario semanal del trabajador es: L. " + totalDevengado);
+
+        // Pausa para que no se cierre la consola automáticamente
+        Console.WriteLine("\nPresione cualquier tecla para salir...");
+        Console.ReadKey();
+    }
+}
